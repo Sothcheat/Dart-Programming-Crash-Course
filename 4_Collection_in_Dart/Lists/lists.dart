bd259e4dc880acb7bfeab315ccacc6baf40c2bed
Sothcheat <sothcheatbunna123@outlook.com>
2025-10-27 09:27:51 +0700

    extra point in list lesson

--- 4_Collection_in_Dart/Lists/lists.dart
+++ 4_Collection_in_Dart/Lists/lists.dart
@@ -37,4 +37,27 @@ void main() {
   // remove from specific location of index
   mixedList.removeAt(2);
   print(mixedList);
+
+  // using where to filter element in list
+  List<String> days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  List<String> startWithS = days
+      .where((element) => element.startsWith("S"))
+      .toList();
+  print(startWithS);
+
+  // filter out even & odd number
+  List<int> numbers = [2, 4, 7, 8, 5, 9, 11, 17, 18];
+  List<int> evenNums = numbers.where((element) => element.isEven).toList();
+  List<int> oddNums = numbers.where((element) => element.isOdd).toList();
+  print(evenNums);
+  print(oddNums);
 }
